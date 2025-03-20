f072ace245f9346cb5b352a5245a64795fde3c1d
Leonardo Almeida <leonardoalmeida7777@gmail.com>
2025-03-20 18:01:01 +0000

    small fix

--- src/flx/cli/preprocess.py
+++ src/flx/cli/preprocess.py
@@ -7,22 +7,22 @@ from flx.cli.utils import get_modules_and_args, load_class
 def main():
 
     FOLDERS: list[str] = [
-        "my_datasets",
+        "datasets",
     ]
 
-    MODULES, ALL_ARGS = get_modules_and_args(FOLDERS)
+    MODULES, _ = get_modules_and_args(FOLDERS)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--dataset", type=str, required=True, help="Dataset name (\"all\" to preprocess all datasets)", choices=["all", *MODULES["my_datasets"].keys()])
+    parser.add_argument("-d", "--dataset", type=str, required=True, help="Dataset name (\"all\" to preprocess all datasets)", choices=["all", *MODULES["datasets"].keys()])
     parser.add_argument("-v", "--val_size", type=float, default=0.15, help="Default: float = 0.15")
     parser.add_argument("-t", "--test_size", type=float, default=0.15, help="Default: float = 0.15")
     args = parser.parse_args()
 
-    datasets = list(MODULES["my_datasets"].keys()) if args.dataset == "all" else [args.dataset]
+    datasets = list(MODULES["datasets"].keys()) if args.dataset == "all" else [args.dataset]
 
     for dataset in datasets: 
         folder = Path(f"Data/{dataset}")
-        dataset = load_class(MODULES["my_datasets"][dataset])()
+        dataset = load_class(MODULES["datasets"][dataset])()
         print(f"\nDataset: {dataset.name}")
         if not folder.exists():
             dataset.download()
