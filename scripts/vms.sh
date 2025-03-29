32a72ce1351608f7fa7c063f468e0bc759d54bfd
Leonardo Almeida <leonardoalmeida7777@gmail.com>
2025-03-29 13:30:57 +0000

    vms

---
+++ scripts/vms.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+export $(grep -v '^#' .env | xargs)
+
+KEY_NAME="fl"
+KEY_PATH="keys/$KEY_NAME"
+VM_LIST="scripts/hostfile.txt"
+USERNAME=$VM_USERNAME
+PASSWORD=$VM_PASSWORD
+
+# Check VM list
+if [ ! -f "$VM_LIST" ]; then
+    echo "Error: VM list file '$VM_LIST' not found!"
+    exit 1
+fi
+
+# Generate SSH key if it doesn't exist
+if [ ! -f "$KEY_PATH" ]; then
+    echo "Generating SSH key..."
+    mkdir -p keys
+    ssh-keygen -t rsa -b 4096 -f "$KEY_PATH" -N "" > /dev/null 2>&1
+    echo "SSH key generated at $KEY_PATH"
+fi
+
+# Copy SSH key to each VM
+while read -r IP; do
+    echo "Adding SSH key to $IP..."
+    ssh-keyscan -H "$IP" >> ~/.ssh/known_hosts > /dev/null 2>&1
+    echo "  IP added to known_hosts"
+    sshpass -p "$PASSWORD" ssh-copy-id -f -i "$KEY_PATH.pub" "$USERNAME@$IP" > /dev/null 2>&1
+    echo "  SSH pubkey added to $IP"
+    sshpass -p "$PASSWORD" scp "$KEY_PATH" "$USERNAME@$IP:~/.ssh/"
+    echo "  SSH privkey copied to $IP"
+    sshpass -p "$PASSWORD" ssh "$USERNAME@$IP" "chmod 600 ~/.ssh/$KEY_NAME"
+    echo "  SSH privkey permissions set on $IP"
+    sshpass -p "$PASSWORD" ssh "$USERNAME@$IP" "touch ~/.ssh/known_hosts"
+    echo "  known_hosts file created on $IP"
+done < "$VM_LIST"
+
+# Make sure VMs can SSH into each other
+echo "Setting up SSH access between VMs..."
+while read -r IP; do
+    for INNER_IP in $(cat "$VM_LIST"); do
+        if [ "$IP" == "$INNER_IP" ]; then
+            continue
+        fi
+        sshpass -p "$PASSWORD" ssh "$USERNAME@$IP" "ssh-keyscan -H $INNER_IP >> ~/.ssh/known_hosts"
+    done
+done < "$VM_LIST"
+
+echo "SSH setup completed!"
--- terraform/main.tf
+++ terraform/main.tf
@@ -15,10 +15,10 @@ variable "pm_password" {}
 
 provider "proxmox" {
     pm_api_url          = var.pm_api_url
-    pm_api_token_id     = var.pm_api_token_id
-    pm_api_token_secret = var.pm_api_token_secret
-    # pm_user             = var.pm_user
-    # pm_password         = var.pm_password
+    # pm_api_token_id     = var.pm_api_token_id
+    # pm_api_token_secret = var.pm_api_token_secret
+    pm_user             = var.pm_user
+    pm_password         = var.pm_password
     pm_tls_insecure     = true
 }
 
